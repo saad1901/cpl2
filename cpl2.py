de0cf9b01465e5074a8ba408872333d1b28dd32b
saadWindows <saadiqbal1921@gmail.com>
2024-04-24 01:19:03 +0530

    Auto Committed

---
+++ cpl2.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import os
+
+def read_file_content(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
+
+# Define the folder from which you want to display files
+folder_path = "./cpl2"  # Update with your folder name
+
+# Get list of valid file extensions
+valid_extensions = ('txt', 'py', 'cpp', 'java', 'json', 'js', 'html', 'css', 'bat', 'c', 'kt')
+
+# List files in the specified folder with valid extensions
+files = [''] + [file for file in os.listdir(folder_path)]
+
+selected_file = st.selectbox('Select file', files)
+
+if selected_file:
+    if selected_file != '':
+        file_path = os.path.join(folder_path, selected_file)
+        st.text(f"{selected_file}")
+        content = read_file_content(file_path)
+        st.code(content, language='python')
+    else:
+        st.info("Select a file from the dropdown to view its content.")
+else:
+    st.text("Please select a file from the dropdown.")
