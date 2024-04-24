e564def864f19ab2a40c057268c37f4dee003731
Swarali <sw04spathrikar@gmail.com>
2024-04-24 23:48:34 +0530

    Auto Committed

---
+++ cns/diffieh.py
@@ -0,0 +1,54 @@
+def prime_checker(p):
+    if p<1:
+        return -1
+    elif p>1:
+        if p ==2:
+            return 1
+        for i in range(2,p):
+            if p%i ==0:
+                return -1
+            return 1
+        
+def primitive_check(g,p,L):
+    for i in range(1,p):
+        L.append(pow(g,i)%p)
+    for i in range(1,p):
+        if L.count(i)>1:
+            L.clear()
+            return -1
+        return 1
+    
+l=[]                
+while l:
+    P= int(input("Enter P: "))
+    if prime_checker(P)==-1:
+        print("Number is not prime, Please enter again!")
+        continue
+    break
+
+while l:
+    G= int(input(f"Enter the primitive root of {P}: "))
+    if primitive_check(G, P, l)==-1:
+        print(f"Number is not a primitive root of {P},please try again!")
+        continue
+    break
+
+while l:
+    Alice, Bob ,Eve=int(input("Enter the primitive key of Alice : ")), int(input("Enter the private key of Bob : ")), int(input("Enter private key of Eve : "))
+    if Alice>=P or Bob>=P or Eve>=P:
+        print(f"Private key of both users should be less than {P}!")
+        continue
+    break
+
+#calculate public keys
+y1, y2, y3=pow(G, Alice)%P, pow(G, Bob)%P, pow(G, Eve)%P
+
+#generate secret keys
+aliceGenerated, bobGenerated, eveGeneratedfromAlice, eveGeneratedfromBob =pow(y3, Alice)%P, pow(y3, Bob)%P, pow(y1, Eve)%P, pow(y2,Eve)%P
+
+print(aliceGenerated, bobGenerated, eveGeneratedfromAlice, eveGeneratedfromBob)
+
+if aliceGenerated ==eveGeneratedfromAlice and bobGenerated==eveGeneratedfromBob:
+    print("Keys have been exchanged successfully")
+else:
+    print("Keys have not been exchanges successfully")    
