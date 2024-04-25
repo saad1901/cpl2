a2045775cb2f9c27ca2f5a81d00b3363e059685b
Swarali <sw04spathrikar@gmail.com>
2024-04-25 08:49:19 +0530

    Auto Committed

--- cns/diffieh.py
+++ cns/diffieh.py
@@ -1,58 +1,61 @@
 def prime_checker(p):
-    if p < 2:
-        return -1
-    elif p > 2:
-        for i in range(2, int(p**0.5) + 1):
-            if p % i == 0:
-                return -1
-    return 1
+	# Checks If the number entered is a Prime Number or not
+	if p < 1:
+		return -1
+	elif p > 1:
+		if p == 2:
+			return 1
+		for i in range(2, p):
+			if p % i == 0:
+				return -1
+			return 1
+
 
 def primitive_check(g, p, L):
-    for i in range(1, p):
-        L.append(pow(g, i) % p)
-    for i in range(1, p):
-        if L.count(i) != 1:
-            L.clear()
-            return -1
-    return 1
+	# Checks If The Entered Number Is A Primitive Root Or Not
+	for i in range(1, p):
+		L.append(pow(g, i) % p)
+	for i in range(1, p):
+		if L.count(i) > 1:
+			L.clear()
+			return -1
+		return 1
 
-while True:
-    P = int(input("Enter P: "))
-    if prime_checker(P) == -1:
-        print("Number is not prime, Please enter again!")
-    else:
-        break
 
 l = []
-while True:
-    G = int(input(f"Enter the primitive root of {P}: "))
-    if primitive_check(G, P, l) == -1:
-        print(f"Number is not a primitive root of {P}, please try again!")
-    else:
-        break
+while 1:
+	P = int(input("Enter P : "))
+	if prime_checker(P) == -1:
+		print("Number Is Not Prime, Please Enter Again!")
+		continue
+	break
 
-while True:
-    Alice = int(input("Enter the private key of Alice: "))
-    Bob = int(input("Enter the private key of Bob: "))
-    Eve = int(input("Enter private key of Eve: "))
-    if Alice >= P or Bob >= P or Eve >= P:
-        print(f"Private key of all users should be less than {P}!")
-    else:
-        break
+while 1:
+	G = int(input(f"Enter The Primitive Root Of {P} : "))
+	if primitive_check(G, P, l) == -1:
+		print(f"Number Is Not A Primitive Root Of {P}, Please Try Again!")
+		continue
+	break
 
+# Private Keys
 
-y1, y2, y3 = pow(G, Alice) % P, pow(G, Bob) % P, pow(G, Eve) % P
+while 1:
+	Alice , Bob,Eve = int(input("Enter The Private Key Of Alice  : ")), int(
+	input("Enter The Private Key Of Bob : ")),int(input("Enter The Private Key Of Eve : "))
+	if Alice >= P or Bob >= P or Eve>=P:
+		print(f"Private Key Of Both The Users Should Be Less Than {P}!")
+		continue
+	break
 
+# Calculate Public Keys
+y1, y2 ,y3= pow(G, Alice) % P, pow(G, Bob) % P ,pow(G, Eve) % P 
 
-aliceGenerated, bobGenerated = pow(y2, Alice) % P, pow(y1, Bob) % P
-eveGeneratedfromAlice, eveGeneratedfromBob = pow(y1, Eve) % P, pow(y2, Eve) % P
+# Generate Secret Keys
+aliceGenerated, bobGenerated ,eveGeneratedfromAlice,eveGeneratedfromBob = pow(y3, Alice) % P, pow(y3, Bob) % P ,pow(y1,Eve)% P,pow(y2,Eve)%P
 
-print("Alice's Generated Key:", aliceGenerated)
-print("Bob's Generated Key:", bobGenerated)
-print("Eve's Generated Key from Alice:", eveGeneratedfromAlice)
-print("Eve's Generated Key from Bob:", eveGeneratedfromBob)
+print(aliceGenerated, bobGenerated ,eveGeneratedfromAlice,eveGeneratedfromBob)
 
-if aliceGenerated == eveGeneratedfromAlice and bobGenerated == eveGeneratedfromBob:
-    print("Keys have been exchanged successfully")
+if aliceGenerated == eveGeneratedfromAlice and bobGenerated==eveGeneratedfromBob:
+ 	print("Keys Have Been Exchanged Successfully")
 else:
-    print("Keys have not been exchanged successfully")
+	print("Keys Have Not Been Exchanged Successfully")
